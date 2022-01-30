b290c992ee0dd0719d326e7cdc5447d96a8b18b3
Valery Zinchenko <minicablestone@gmail.com>
2022-01-30 05:56:46 +0300

    add icons add Icon component

---
+++ src/app/components/common/Icon/Icon.tsx
@@ -0,0 +1,20 @@
+import { SVGAttributes } from "react"
+import { classMerge, classWithModifiers } from "utils/common"
+
+
+export type IconName = ("language" | "drop-down-triangle") | (string & {})
+
+interface IconProps extends SVGAttributes<SVGElement> {
+  name: IconName
+}
+
+function Icon(props: IconProps) {
+  return (
+    <svg {...props} className={classMerge(classWithModifiers("icon", props.name), props.className)}>
+      <use href={`/static/icons.svg#${props.name}`} />
+    </svg>
+  )
+}
+
+
+export default Icon
