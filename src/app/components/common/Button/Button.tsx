d7399f62a8cdca82107f9e75be7c7616f5d65a20
Valery Zinchenko <minicablestone@gmail.com>
2022-01-30 19:10:50 +0300

    Button update

--- src/app/components/common/Button/Button.tsx
+++ src/app/components/common/Button/Button.tsx
@@ -8,7 +8,8 @@ import { classWithModifiers } from "utils/common"
 interface ButtonProps {
   size?: "small" | "big"
   style?: "outline"
-  color?: "green"
+  color?: "green" | "violet"
+  type?: "reset" | "submit"
 
   iconLeft?: ReactNode
   iconRight?: ReactNode
@@ -19,7 +20,7 @@ interface ButtonProps {
 
 function Button(props: ButtonProps) {
   return (
-    <button className={classWithModifiers("button", props.style, props.size, props.color)} type="button" onClick={props.onClick}>
+    <button className={classWithModifiers("button", props.style, props.size, props.color)} type={props.type || "button"} onClick={props.onClick}>
       <div className="button__icon">{props.iconLeft}</div>
       <div className="button__text">{props.children}</div>
       <div className="button__icon">{props.iconRight}</div>
