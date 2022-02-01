0bf2d6e2a0f6b581835c507a5756295de976e2fe
Valery Zinchenko <minicablestone@gmail.com>
2022-02-01 16:05:27 +0300

    add OuterLink

---
+++ src/app/components/services/OuterLink.tsx
@@ -0,0 +1,13 @@
+import { HTMLAttributes } from "react"
+
+
+interface OuterLinkProps extends Omit<HTMLAttributes<HTMLAnchorElement>, "href" | "rel" | "target"> {
+  to: string
+}
+
+function OuterLink(props: OuterLinkProps) {
+  return <a {...props} rel="noopener noreferrer" target="_blank" href={props.to} />
+}
+
+
+export default OuterLink
