a23957233533ac6d94481afe77e52198c7359940
Valery Zinchenko <minicablestone@gmail.com>
2022-02-02 13:39:54 +0300

    fix #14

--- src/app/components/other/MentorSearch/MentorSearch.tsx
+++ src/app/components/other/MentorSearch/MentorSearch.tsx
@@ -4,47 +4,150 @@ import Button from "app/components/common/Button/Button"
 import Icon from "app/components/common/Icon/Icon"
 import TopicTag from "app/components/UI/Tag/TopicTag"
 import useLocalization from "modules/localization/hook"
-import { ChangeEvent, Dispatch, useState } from "react"
+import { ChangeEvent, Dispatch, useRef, useState } from "react"
 import { classWithModifiers } from "utils/common"
 
 
+const SCROLL_STEP = window.innerWidth / 500
+const SCROLL_INTERVAL = 5
+
 function MentorSearch() {
   const ll = useLocalization(ll => ll.views.home.mentorSearch)
+  const intervalRef = useRef<NodeJS.Timer>()
+  const tagsInnerRef = useRef<HTMLDivElement | null>(null)
   const [value, setValue] = useState("")
   const [picks, setPicks] = useState<string[]>([])
   function onChange(event: ChangeEvent<HTMLInputElement>) {
     const value = event.currentTarget.value
     setValue(value)
   }
+  function onPick(pick: string) {
+    setPicks([...picks, pick])
+  }
+  function onLeftShadowEnter() {
+    intervalRef.current = setInterval(() => {
+      if (!tagsInnerRef.current) return
+      tagsInnerRef.current.scrollBy(-SCROLL_STEP, 0)
+    }, SCROLL_INTERVAL)
+  }
+  function onRightShadowEnter() {
+    intervalRef.current = setInterval(() => {
+      if (!tagsInnerRef.current) return
+      tagsInnerRef.current.scrollBy(SCROLL_STEP, 0)
+    }, SCROLL_INTERVAL)
+  }
+  function onShadowBlur() {
+    if (!intervalRef.current) return
+    clearInterval(intervalRef.current)
+  }
   return (
     <div className="mentor-search">
       <div className="mentor-search__container">
-        <label className={classWithModifiers("mentor-search__search", !!value.length && "filled")}>
+        <div className={classWithModifiers("mentor-search__search", !!value.length && "filled")}>
           {picks.map(pick => (
             <TopicTag key={pick}>{pick}</TopicTag>
           ))}
           <input type="text" placeholder={picks.length === 0 ? ll.placeholder : undefined} className="mentor-search__input" onChange={onChange} />
           <Icon name="chevron" className="mentor-search__icon" />
-          <MentorSearchList searchValue={value} onChange={pick => setPicks([...picks, pick])} />
-        </label>
+          <MentorSearchList searchValue={value} onChange={onPick} />
+        </div>
         <Button color="violet" size="big">{ll.button}</Button>
       </div>
       <div className="mentor-search__tags">
-        <TopicTag>Иллюстрация</TopicTag>
-        <TopicTag>Архитектура</TopicTag>
-        <TopicTag>Графический дизайн</TopicTag>
-        <TopicTag>Fashion дизайн</TopicTag>
-        <TopicTag>UX/UI</TopicTag>
-        <TopicTag>Иллюстрация</TopicTag>
-        <TopicTag>Архитектура</TopicTag>
-        <TopicTag>Графический дизайн</TopicTag>
-        <TopicTag>Fashion дизайн</TopicTag>
-        <TopicTag>UX/UI</TopicTag>
-        <TopicTag>Иллюстрация</TopicTag>
-        <TopicTag>Архитектура</TopicTag>
-        <TopicTag>Графический дизайн</TopicTag>
-        <TopicTag>Fashion дизайн</TopicTag>
-        <TopicTag>UX/UI</TopicTag>
+        <div className="mentor-search__shadow mentor-search__shadow--left" onPointerEnter={onLeftShadowEnter} onPointerLeave={onShadowBlur} />
+        <div className="mentor-search__shadow mentor-search__shadow--right" onPointerEnter={onRightShadowEnter} onPointerLeave={onShadowBlur} />
+        <div className="mentor-search__inner-tags" ref={tagsInnerRef}>
+          <TopicTag>Иллюстрация</TopicTag>
+          <TopicTag>Архитектура</TopicTag>
+          <TopicTag>Графический дизайн</TopicTag>
+          <TopicTag>Fashion дизайн</TopicTag>
+          <TopicTag>UX/UI</TopicTag>
+          <TopicTag>Иллюстрация</TopicTag>
+          <TopicTag>Архитектура</TopicTag>
+          <TopicTag>Графический дизайн</TopicTag>
+          <TopicTag>Fashion дизайн</TopicTag>
+          <TopicTag>UX/UI</TopicTag>
+          <TopicTag>Иллюстрация</TopicTag>
+          <TopicTag>Архитектура</TopicTag>
+          <TopicTag>Графический дизайн</TopicTag>
+          <TopicTag>Fashion дизайн</TopicTag>
+          <TopicTag>UX/UI</TopicTag>
+          <TopicTag>Иллюстрация</TopicTag>
+          <TopicTag>Архитектура</TopicTag>
+          <TopicTag>Графический дизайн</TopicTag>
+          <TopicTag>Fashion дизайн</TopicTag>
+          <TopicTag>UX/UI</TopicTag>
+          <TopicTag>Иллюстрация</TopicTag>
+          <TopicTag>Архитектура</TopicTag>
+          <TopicTag>Графический дизайн</TopicTag>
+          <TopicTag>Fashion дизайн</TopicTag>
+          <TopicTag>UX/UI</TopicTag>
+          <TopicTag>Иллюстрация</TopicTag>
+          <TopicTag>Архитектура</TopicTag>
+          <TopicTag>Графический дизайн</TopicTag>
+          <TopicTag>Fashion дизайн</TopicTag>
+          <TopicTag>UX/UI</TopicTag>
+          <TopicTag>Иллюстрация</TopicTag>
+          <TopicTag>Архитектура</TopicTag>
+          <TopicTag>Графический дизайн</TopicTag>
+          <TopicTag>Fashion дизайн</TopicTag>
+          <TopicTag>UX/UI</TopicTag>
+          <TopicTag>Иллюстрация</TopicTag>
+          <TopicTag>Архитектура</TopicTag>
+          <TopicTag>Графический дизайн</TopicTag>
+          <TopicTag>Fashion дизайн</TopicTag>
+          <TopicTag>UX/UI</TopicTag>
+          <TopicTag>Иллюстрация</TopicTag>
+          <TopicTag>Архитектура</TopicTag>
+          <TopicTag>Графический дизайн</TopicTag>
+          <TopicTag>Fashion дизайн</TopicTag>
+          <TopicTag>UX/UI</TopicTag>
+          <TopicTag>Иллюстрация</TopicTag>
+          <TopicTag>Архитектура</TopicTag>
+          <TopicTag>Графический дизайн</TopicTag>
+          <TopicTag>Fashion дизайн</TopicTag>
+          <TopicTag>UX/UI</TopicTag>
+          <TopicTag>Иллюстрация</TopicTag>
+          <TopicTag>Архитектура</TopicTag>
+          <TopicTag>Графический дизайн</TopicTag>
+          <TopicTag>Fashion дизайн</TopicTag>
+          <TopicTag>UX/UI</TopicTag>
+          <TopicTag>Иллюстрация</TopicTag>
+          <TopicTag>Архитектура</TopicTag>
+          <TopicTag>Графический дизайн</TopicTag>
+          <TopicTag>Fashion дизайн</TopicTag>
+          <TopicTag>UX/UI</TopicTag>
+          <TopicTag>Иллюстрация</TopicTag>
+          <TopicTag>Архитектура</TopicTag>
+          <TopicTag>Графический дизайн</TopicTag>
+          <TopicTag>Fashion дизайн</TopicTag>
+          <TopicTag>UX/UI</TopicTag>
+          <TopicTag>Иллюстрация</TopicTag>
+          <TopicTag>Архитектура</TopicTag>
+          <TopicTag>Графический дизайн</TopicTag>
+          <TopicTag>Fashion дизайн</TopicTag>
+          <TopicTag>UX/UI</TopicTag>
+          <TopicTag>Иллюстрация</TopicTag>
+          <TopicTag>Архитектура</TopicTag>
+          <TopicTag>Графический дизайн</TopicTag>
+          <TopicTag>Fashion дизайн</TopicTag>
+          <TopicTag>UX/UI</TopicTag>
+          <TopicTag>Иллюстрация</TopicTag>
+          <TopicTag>Архитектура</TopicTag>
+          <TopicTag>Графический дизайн</TopicTag>
+          <TopicTag>Fashion дизайн</TopicTag>
+          <TopicTag>UX/UI</TopicTag>
+          <TopicTag>Иллюстрация</TopicTag>
+          <TopicTag>Архитектура</TopicTag>
+          <TopicTag>Графический дизайн</TopicTag>
+          <TopicTag>Fashion дизайн</TopicTag>
+          <TopicTag>UX/UI</TopicTag>
+          <TopicTag>Иллюстрация</TopicTag>
+          <TopicTag>Архитектура</TopicTag>
+          <TopicTag>Графический дизайн</TopicTag>
+          <TopicTag>Fashion дизайн</TopicTag>
+          <TopicTag>UX/UI</TopicTag>
+        </div>
       </div>
     </div>
   )
@@ -72,7 +175,16 @@ function MentorSearchList(props: MentorSearchListProps) {
 }
 
 
-const staticTopicNames = ["architecture", "design", "art", "craft-jewellery", "games-animation", "movies-tv-photos", "fashion-design", "music-sound", "theater-scene-art", "digital-design", "art-management", "journalism-writing"]
+function MentorSearchListItem(props: { name: string }) {
+  const ll = useLocalization(ll => ll.views.home.mentorSearch)
+  return (
+    <div className="mentor-search-list__item">
+      <Icon className="mentor-search-list__icon" name={props.name} />
+      <span>{ll.staticTopics[props.name]}</span>
+    </div>
+  )
+}
+
 
 interface MentorSearchStaticListProps {
   onChange: Dispatch<string>
@@ -83,7 +195,7 @@ function MentorSearchStaticList(props: MentorSearchStaticListProps) {
   return (
     <div className="mentor-search-list">
       <div className="mentor-search-list__container">
-        {staticTopicNames.map((topic, index) => (
+        {Object.keys(ll.staticTopics).map((topic, index) => (
           <button className="mentor-search-list__item" type="button" onClick={() => props.onChange(topic)} key={index}>{ll.staticTopics[topic]}</button>
         ))}
       </div>
