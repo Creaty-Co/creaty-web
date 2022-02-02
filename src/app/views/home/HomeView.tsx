a652af9dac497dd50775305aef71c5ca688208c6
Valery Zinchenko <minicablestone@gmail.com>
2022-02-03 00:57:54 +0300

    HomeView update

--- src/app/views/home/HomeView.tsx
+++ src/app/views/home/HomeView.tsx
@@ -3,6 +3,7 @@ import "./HomeView.scss"
 import HaveQuestions from "app/components/other/HaveQuestions/HaveQuestions"
 import HelpSocial from "app/components/other/HelpSocial/HelpSocial"
 import MentorSearch from "app/components/other/MentorSearch/MentorSearch"
+import MentorSearchTags from "app/components/other/MentorSearch/MentorSearchTags"
 import MentorsSlider from "app/components/other/MentorsSlider/MentorsSlider"
 import BigComment from "app/components/UI/BigComment/BigComment"
 import { FAQ, FAQClause } from "app/components/UI/FAQ/FAQ"
@@ -15,13 +16,17 @@ import HelpfulCreaty from "./HelpfulCreaty/HelpfulCreaty"
 import HowItWorks from "./HowItWorks/HowItWorks"
 import MailingSubscribe from "./MailingSubscribe/MailingSubscribe"
 
+
 function HomeView() {
   const ll = useLocalization(ll => ll.views.home)
   return (
     <div className="home-view">
       <div className="home-view__header">
         <DynamicPrimaryInfo />
-        <MentorSearch />
+        <div className="home-view__search">
+          <MentorSearch />
+        </div>
+        <MentorSearchTags />
       </div>
       <div className="home-view__comment">
         <BigComment>{ll.bigComment}</BigComment>
@@ -30,7 +35,7 @@ function HomeView() {
         <MentorsSlider />
       </div>
       <div className="home-view__help">
-        <InfoSection type="2" display="flex" title="Не знаете с чего начать?" desc="Напишите нам: поможем с выбором ментора и ответим на все ваши вопросы">
+        <InfoSection type="2" display="flex" {...ll.help}>
           <HelpSocial />
         </InfoSection>
       </div>
@@ -58,20 +63,11 @@ function HomeView() {
 }
 
 
-const headings = [
-  "Fashion дизайн",
-  "UX дизайн",
-  "Иллюстрация",
-  "UX/UI",
-  "Архитектура",
-  "Ещё какой-то очень длинный тэг",
-  "BIM",
-  "Архитетура малых форм",
-  "Дизайн архитектурной среды",
-  "Дизайн интерьеров"
-]
 function DynamicPrimaryInfo() {
+  const staticTopics = useLocalization(ll => ll.views.home.mentorSearch.staticTopics)
+  const headings = Object.values(staticTopics)
   const ll = useLocalization(ll => ll.views.home.primaryInfo)
+
   const rejectRef = useRef<Function>()
   const [currentHeading, setCurrentHeading] = useState(0)
   const [dynamicHeading, setDynamicHeading] = useState(headings[currentHeading])
@@ -115,7 +111,7 @@ function DynamicPrimaryInfo() {
     })
 
     return () => rejectRef.current?.("DynamicPrimaryInfo was unmounted")
-  }, [currentHeading])
+  }, [currentHeading, staticTopics])
   return (
     <div className="dynamic-primary-info">
       <h1 className="dynamic-primary-info__title">
@@ -144,7 +140,6 @@ function QAndA() {
     </FAQ>
   )
 }
-
 
 
 export default HomeView
