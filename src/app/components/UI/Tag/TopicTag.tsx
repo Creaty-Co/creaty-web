43f17a202c2004a20bd6d18bbeb1fb1b4a8a5e3d
Valery Zinchenko <minicablestone@gmail.com>
2022-02-03 03:52:52 +0300

    fix #15

--- src/app/components/UI/Tag/TopicTag.tsx
+++ src/app/components/UI/Tag/TopicTag.tsx
@@ -1,18 +1,37 @@
 import "./TopicTag.scss"
 
+import useLocalization from "modules/localization/hook"
+import { MouseEventHandler } from "react"
+import { Link } from "react-router-dom"
+import { SearchTag } from "redux/reducers/search"
 import { classWithModifiers } from "utils/common"
 
 
-interface TopicTagProps {
+interface TopicTagStringProps {
   noHash?: boolean
   children: string
+  onClick?: MouseEventHandler<HTMLButtonElement>
 }
 
-function TopicTag(props: TopicTagProps) {
+interface TopicTagSearchTopicProps {
+  noHash?: undefined
+  children: SearchTag
+  onClick?: undefined
+}
+
+function TopicTag(props: TopicTagStringProps | TopicTagSearchTopicProps) {
+  const tags = useLocalization(ll => ll.other.tags)
+  if (typeof props.children === "string") {
+    return (
+      <button className={classWithModifiers("topic-tag", props.noHash && "no-hash")} type="button" onClick={props.onClick}>
+        <span className="topic-tag__text">{props.children}</span>
+      </button>
+    )
+  }
   return (
-    <button className={classWithModifiers("topic-tag", props.noHash && "no-hash")} type="button">
-      <span className="topic-tag__text">{props.children}</span>
-    </button>
+    <Link className="topic-tag" to={`/mentors/${props.children.name}/`}>
+      <span className="topic-tag__text">{tags[props.children.name] || props.children.name}</span>
+    </Link>
   )
 }
 
