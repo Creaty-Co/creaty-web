a735fa7a416b3f6d07e9d448123022c6dcd720ba
Valery Zinchenko <minicablestone@gmail.com>
2022-02-17 19:51:50 +0300

    Buffer fix

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,7 @@ import App from "app/App"
 import { createElement } from "react"
 import ReactDOM from "react-dom"
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+window.Buffer = require("buffer").Buffer
+
 ReactDOM.render(createElement(App), document.getElementById("root"))
