e2fd06638b30a5e1be2051fc3e980a7d96957408
Valery Zinchenko <minicablestone@gmail.com>
2022-02-21 00:39:51 +0300

    fix #91

--- src/app/components/other/MentorSearch/MentorSearchTags.tsx
+++ src/app/components/other/MentorSearch/MentorSearchTags.tsx
@@ -1,46 +1,24 @@
 import TopicTag from "app/components/UI/Tag/TopicTag"
 import { TagType } from "interfaces/types"
-import { useRef } from "react"
-
-
-const SCROLL_STEP = window.innerWidth / 500
-const SCROLL_INTERVAL = 5
+import { useState } from "react"
 
 interface MentorSearchTagsProps {
   tags: TagType[]
 }
 
-export default function MentorSearchTags(props: MentorSearchTagsProps) {
-  const intervalRef = useRef<NodeJS.Timer>()
-  const tagsInnerRef = useRef<HTMLDivElement | null>(null)
-  function onLeftShadowEnter() {
-    intervalRef.current = setInterval(() => {
-      if (!tagsInnerRef.current)
-        return
-      tagsInnerRef.current.scrollBy(-SCROLL_STEP, 0)
-    }, SCROLL_INTERVAL)
-  }
-  function onRightShadowEnter() {
-    intervalRef.current = setInterval(() => {
-      if (!tagsInnerRef.current)
-        return
-      tagsInnerRef.current.scrollBy(SCROLL_STEP, 0)
-    }, SCROLL_INTERVAL)
-  }
-  function onShadowBlur() {
-    if (!intervalRef.current)
-      return
-    clearInterval(intervalRef.current)
-  }
+function MentorSearchTags(props: MentorSearchTagsProps) {
+  const [innerWidth, setInnerWidth] = useState<number>()
   return (
-    <div className="mentor-search__tags">
-      <div className="mentor-search__inner-tags" ref={tagsInnerRef}>
-        {props.tags.map(tag => (
+    <div className="mentor-search__tags" style={{ "--inner-width": innerWidth }}>
+      <div className="mentor-search__inner-tags" ref={el => setInnerWidth(el?.scrollWidth)}>
+        {[...props.tags, ...props.tags].map(tag => (
           <TopicTag key={tag.id}>{tag}</TopicTag>
         ))}
       </div>
-      <div className="mentor-search__shadow mentor-search__shadow--left" onPointerEnter={onLeftShadowEnter} onPointerLeave={onShadowBlur} />
-      <div className="mentor-search__shadow mentor-search__shadow--right" onPointerEnter={onRightShadowEnter} onPointerLeave={onShadowBlur} />
+      <div className="mentor-search__shadow mentor-search__shadow--left" />
+      <div className="mentor-search__shadow mentor-search__shadow--right" />
     </div>
   )
 }
+
+export default MentorSearchTags
