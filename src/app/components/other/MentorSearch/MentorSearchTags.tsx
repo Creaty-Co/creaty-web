b513e35121f8a2ab367bb88c591bcff21153878d
Valery Zinchenko <minicablestone@gmail.com>
2022-02-24 00:59:38 +0300

    patch

--- src/app/components/other/MentorSearch/MentorSearchTags.tsx
+++ src/app/components/other/MentorSearch/MentorSearchTags.tsx
@@ -1,16 +1,16 @@
 import TopicTag from "app/components/UI/Tag/TopicTag"
 import { TagType } from "interfaces/types"
-import { useState } from "react"
+import { useRef } from "react"
 
 interface MentorSearchTagsProps {
   tags: TagType[]
 }
 
 function MentorSearchTags(props: MentorSearchTagsProps) {
-  const [innerWidth, setInnerWidth] = useState<number>()
+  const innerRef = useRef<HTMLDivElement>(null)
   return (
-    <div className="mentor-search__tags" style={{ "--inner-width": innerWidth }}>
-      <div className="mentor-search__inner-tags" ref={el => setInnerWidth(el?.scrollWidth)}>
+    <div className="mentor-search__tags" style={{ "--inner-width": innerRef.current?.scrollWidth }}>
+      <div className="mentor-search__inner-tags" ref={innerRef}>
         {[...props.tags, ...props.tags].map(tag => (
           <TopicTag key={tag.id}>{tag}</TopicTag>
         ))}
