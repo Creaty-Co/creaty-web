b13930f665fadb47f24f7273fc709493ce4d487c
FrameMuse <minicablestone@gmail.com>
2022-04-16 01:22:54 +0300

    fix #177 #132

--- src/app/components/other/MentorSearch/MentorSearchTags.tsx
+++ src/app/components/other/MentorSearch/MentorSearchTags.tsx
@@ -11,7 +11,16 @@ interface MentorSearchTagsProps {
 function MentorSearchTags(props: MentorSearchTagsProps) {
   const innerRef = useRef<HTMLDivElement>(null)
   const [width, setWidth] = useState<number>()
-  useEffect(() => setWidth(innerRef.current?.scrollWidth), [props.tags])
+  useEffect(() => {
+    (() => {
+      const scrollWidth = innerRef.current?.scrollWidth
+      const clientWidth = innerRef.current?.clientWidth
+      const offsetWidth = innerRef.current?.offsetWidth
+      const rectWidth = innerRef.current?.getBoundingClientRect().width
+      // Safari compatibility
+      setWidth(scrollWidth || clientWidth || offsetWidth || rectWidth || window.innerWidth || window.outerWidth || 2000)
+    })()
+  }, [props.tags])
   return (
     <div className="mentor-search__tags" style={{ "--inner-width": width }}>
       <div className="mentor-search__inner-tags" ref={innerRef}>
