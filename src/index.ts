5820e3b0f96aaf1bfc841cb9d3581c80feabc987
FrameMuse <minicablestone@gmail.com>
2022-04-17 11:40:05 +0300

    add pwa

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import App from "app/App"
 import { createElement } from "react"
 import { createRoot } from "react-dom/client"
+import * as serviceWorkerRegistration from "services/serviceWorkerRegistration"
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 window.Buffer = require("buffer").Buffer
 
-function init() {
-  const rootElement = document.getElementById("root")
-  if (rootElement === null) return
-
+const rootElement = document.getElementById("root")
+if (rootElement) {
   const root = createRoot(rootElement)
   root.render(createElement(App))
 }
 
-init()
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register()
