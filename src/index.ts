e9fc33a6b57761574b33d735b18bb16b8fb587da
FrameMuse <minicablestone@gmail.com>
2022-04-21 16:27:21 +0300

    unregister service worker

--- src/index.ts
+++ src/index.ts
@@ -15,4 +15,4 @@ if (rootElement) {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register()
+serviceWorkerRegistration.unregister()
