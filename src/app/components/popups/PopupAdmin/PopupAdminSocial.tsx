9b0990ef353e82fa67d04702c9bfd2f8568c0073
FrameMuse <minicablestone@gmail.com>
2022-04-30 05:18:34 +0300

    fix #158

--- src/app/components/popups/PopupAdmin/PopupAdminSocial.tsx
+++ src/app/components/popups/PopupAdmin/PopupAdminSocial.tsx
@@ -1,45 +1,48 @@
 import { deletePagesLinksSocials, patchPagesLinksSocials, postPagesLinksSocials } from "api/actions/pages"
-import ClientAPI from "api/client"
-import { FormElements } from "interfaces/common"
+import Form, { FormState } from "app/components/UI/Form/Form"
 import { PageLinkSocialType } from "interfaces/types"
 import { useModal } from "modules/modal/hook"
-import { FormEvent } from "react"
-import { FileToURLDataBase64 } from "utils/common"
+import { useState } from "react"
+import { useClient } from "react-fetching-library"
+import { toast } from "react-toastify"
 
 import Button from "../../common/Button/Button"
 import Input from "../../UI/Input/Input"
 import PopupLayout from "../PopupLayout"
 
+enum FormInputs {
+  url = "url",
+  icon = "icon"
+}
+interface FormValues {
+  url: string
+  icon?: string
+}
 
 export function PopupAdminNewSocial() {
   const { close } = useModal()
-  async function submitSocial(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
+  const client = useClient()
+  const [pending, setPending] = useState(false)
+  async function onSubmit(state: FormState<FormInputs, FormValues>) {
+    const { url, icon } = state.values
+    if (icon == null) {
+      return toast.error("Вы не выбрали иконку")
+    }
 
-    const target = event.currentTarget
-    const elements = target.elements as FormElements<"url" | "icon">
+    setPending(true)
+    const { error } = await client.query(postPagesLinksSocials({ url, icon }))
+    setPending(false)
+    if (error) return
 
-    const iconFile = elements.icon.files?.[0]
-    if (!iconFile) return
-
-    ClientAPI
-      .query(postPagesLinksSocials({
-        url: elements.url.value,
-        icon: await FileToURLDataBase64(iconFile)
-      }))
-      .then(({ error }) => {
-        if (error) return
-        close()
-        window.location.reload()
-      })
+    close()
   }
   return (
     <PopupLayout title="Добавить ссылку">
-      <form onSubmit={submitSocial}>
-        <Input name="url" placeholder="Внешняя ссылка" />
-        <Input name="icon" type="file" />
-        <Button color="dark" type="submit">Добавить</Button>
-      </form>
+      <Form onSubmit={onSubmit}>
+        <Input name={FormInputs.url} placeholder="Внешняя ссылка" />
+        <Input name={FormInputs.icon} type="file" />
+        <Button color="dark" type="submit" pending={pending}>Добавить</Button>
+      </Form>
     </PopupLayout>
   )
 }
@@ -51,43 +54,30 @@ interface PopupAdminEditSocialProps {
 
 export function PopupAdminEditSocial(props: PopupAdminEditSocialProps) {
   const { close } = useModal()
-  function deleteSocial() {
-    ClientAPI
-      .query(deletePagesLinksSocials(props.img.id))
-      .then(({ error }) => {
-        if (error) return
-        close()
-        window.location.reload()
-      })
+  const client = useClient()
+  const [pending, setPending] = useState(false)
+  async function deleteSocial() {
+    const { error } = await client.query(deletePagesLinksSocials(props.img.id))
+    if (error) return
+
+    close()
   }
-  async function submitSocial(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
+  async function onSubmit(state: FormState<FormInputs, FormValues>) {
+    setPending(true)
+    const { error } = await client.query(patchPagesLinksSocials(props.img.id, state.values))
+    setPending(false)
+    if (error) return
 
-    const target = event.currentTarget
-    const elements = target.elements as FormElements<"url" | "icon">
-
-    const iconFile = elements.icon.files?.[0]
-    if (!iconFile) return
-
-    ClientAPI
-      .query(patchPagesLinksSocials(props.img.id, {
-        url: elements.url.value,
-        icon: await FileToURLDataBase64(iconFile)
-      }))
-      .then(({ error }) => {
-        if (error) return
-        close()
-        window.location.reload()
-      })
+    close()
   }
   return (
-    <PopupLayout title="Редактироть ссылку">
-      <form onSubmit={submitSocial}>
-        <Input name="url" placeholder="Внешняя ссылка" defaultValue={props.img.url} key={props.img.url} />
-        <Input name="icon" type="file" />
-        <Button color="dark" type="submit">Сохранить</Button>
+    <PopupLayout title="Редактировать ссылку">
+      <Form onSubmit={onSubmit}>
+        <Input name={FormInputs.url} placeholder="Внешняя ссылка" defaultValue={props.img.url} key={props.img.url} />
+        <Input name={FormInputs.icon} type="file" />
+        <Button color="dark" type="submit" pending={pending}>Сохранить</Button>
         <Button color="violet" onClick={deleteSocial}>Удалить</Button>
-      </form>
+      </Form>
     </PopupLayout>
   )
 }
