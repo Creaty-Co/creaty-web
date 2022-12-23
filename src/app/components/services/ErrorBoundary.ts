452ba79d5f296475d9720a22cb392e10190af863
prmaximus <prmaximus@yandex.ru>
2022-12-23 16:07:41 +0300

    End Error Boundary

--- src/app/components/services/ErrorBoundary.ts
+++ src/app/components/services/ErrorBoundary.ts
@@ -6,14 +6,22 @@ interface ErrorBoundaryProps {
   fallback: ReactNode
 }
 interface ErrorBoundaryState {
-  error: Error | null
   errorInfo: ErrorInfo | null
+  error: Error | null
+  hasError: boolean
 }
 
 class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
-  state = {
-    error: null,
-    errorInfo: null
+  state : ErrorBoundaryState = {
+    hasError: false,
+    errorInfo: null,
+    error: null
+  }
+
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+    if (error) return { hasError: true, errorInfo: null, error: error }
+
+    return { hasError: false, errorInfo: null, error: null }
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
@@ -23,12 +31,16 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
       description: "An error ocurred",
       errorName: error.name,
       errorMessage: error.message
-    })
+    });
+
+    console.log('error: ', error);
+    console.log('error info:', errorInfo);
+
     this.setState({ error, errorInfo })
   }
 
   render() {
-    if (this.state.error) {
+    if (this.state.hasError) {
       return this.props.fallback
     }
 
--- src/app/views/error/ErrorView.tsx
+++ src/app/views/error/ErrorView.tsx
@@ -2,15 +2,28 @@ import "./ErrorView.scss"
 
 import ButtonLink from "app/components/common/Button/ButtonLink"
 
-function ErrorView() {
+interface ErrorViewProps {
+  error?: Error
+  errorCode?: string | number
+}
+
+function ErrorView({
+  error,
+  errorCode = "404"
+} : ErrorViewProps) {
+  const msg = error? error.message : "Ooops, something goes wrong...";
+  const code = error
+    ? errorCode !== "404"? errorCode : error.name
+    : "404";
+  
   return (
     <div className="error-view">
       <div className="error-view__container">
         <div className="error-view__info">
-          <h2 className="heading error-view__title">Ooops, something goes wrong...</h2>
+          <h2 className="heading error-view__title">{msg}</h2>
           <ButtonLink size="big" color="white" to="/">Back to main</ButtonLink>
         </div>
-        <div className="error-view__code">404</div>
+        <div className="error-view__code">{ code }</div>
       </div>
     </div>
   )
