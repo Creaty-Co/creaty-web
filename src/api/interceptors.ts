2f4d0ef9ec2b10a2a9d4f565412159ad65e286e9
Max <prmaximus@yandex.ru>
2023-01-15 18:44:14 +0300

    fix: Change protocol for work on stage (#18)

--- src/api/interceptors.ts
+++ src/api/interceptors.ts
@@ -14,8 +14,12 @@ export function endpointTransform(action: Action) {
   const API_URL = process.env.REACT_APP_API_HOST
   if (API_URL == null) return ""
 
+  /*
+  For the best times
   const protocol = process.env.NODE_ENV === "development" ? "http" : "https"
+  */
   const url = new URL(API_URL)
+  const protocol = url.protocol
 
   const actionEndpoint = action.endpoint[0] === "/" ? action.endpoint.slice(1) : action.endpoint
   const endpoint = `${protocol}://${url.host}/${actionEndpoint}/`
