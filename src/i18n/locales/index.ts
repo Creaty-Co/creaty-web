5202fb841f356863d51a3bc79b379a7e120ae232
Max Prokhorov <prmaximus@yandex.ru>
2023-03-10 06:40:31 +0300

    fix: Only english (#67)

--- src/i18n/locales/index.ts
+++ src/i18n/locales/index.ts
@@ -2,7 +2,7 @@ import { ExtractArrayType } from "@shared/types"
 
 import LocaleResourceSchemaJson from "./schema.json"
 
-export const supportedLocales = ["ru", "en"] as const
+export const supportedLocales = ["en"] as const
 
 export type LocaleKeys = ExtractArrayType<typeof supportedLocales> | "dev"
 export type LocaleResourceSchema = typeof LocaleResourceSchemaJson
