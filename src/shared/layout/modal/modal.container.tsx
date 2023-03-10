b397127b1b380191f3916e2017393c6438889ab1
prmaximus <prmaximus@yandex.ru>
2023-03-10 13:43:44 +0300

    add: Close modal on Escape

--- src/shared/layout/modal/modal.container.tsx
+++ src/shared/layout/modal/modal.container.tsx
@@ -3,7 +3,7 @@ import "./modal.scss"
 import { useAppDispatch, useAppSelector } from "@app/store"
 import { bem, stopPropagation } from "@shared/utils"
 import cn from "classnames"
-import { useLayoutEffect, useRef } from "react"
+import { useCallback, useLayoutEffect, useRef } from "react"
 
 import { close, selectModalActive, selectModalContent } from "./modal.slice"
 
@@ -23,17 +23,26 @@ export function ModalContainer({
   const elementContent = useAppSelector(selectModalContent())
   const isActive = useAppSelector(selectModalActive())
 
+  const handleEscKey = useCallback((event: KeyboardEvent) => {
+    console.log("event.code", event.code)
+    if (event.code === "Escape") dispatch(close()) 
+  }, [isActive])
+
   useLayoutEffect(() => {
     if (isActive) {
       scrollTopOffsetRef.current = window.scrollY
       document.body.style.position = "fixed"
       document.body.style.top = `-${scrollTopOffsetRef.current}px`
+
+      window.addEventListener("keydown", handleEscKey)
+      return () => window.removeEventListener("keydown", handleEscKey)
     } else {
       document.body.style.position = ""
       document.body.style.top = ""
       window.scrollTo(0, parseInt(`${scrollTopOffsetRef.current}` || "0") )
     }
   }, [isActive])
+
 
   return (
     <div className={cn(
