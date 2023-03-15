449f73971b527e589027ab00ea74c3e3d45f82f5
prmaximus <prmaximus@yandex.ru>
2023-03-15 11:24:10 +0300

    fix: Mentros

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -2,13 +2,14 @@
 import { Route, Routes } from "react-router"
 
 import { Home } from "./home"
+import { Mentors } from "./mentors"
 
 export const Router = () => (
   <Routes>
     <Route path="/">
       <Route index element={<Home />} />
       {/* <Route path=":shortcut" element={<Home/>} /> */}
-      {/* <Route path="mentors" element={<Mentors/>} /> */}
+      <Route path="mentors" element={<Mentors/>} />
       {/* <Route path="mentors/:topicOrTag" element={<Mentors/>} /> */}
       {/* <Route path="mentor/:slug" element={<Mentor/>} /> */}
       {/* <Route path="user/:slug" element={<Mentor/>} /> */}
