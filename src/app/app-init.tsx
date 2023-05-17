62cdaf369f5c72dfce02931f3ae4163435cc813f
Dmitrii Doroshenko <DmitriyD@radware.com>
2023-05-17 15:31:39 +0300

    delete excess alert

--- src/app/app-init.tsx
+++ src/app/app-init.tsx
@@ -14,7 +14,6 @@ if (process.env.REACT_APP_API_GA) {
   ReactGA.initialize(process.env.REACT_APP_API_GA)
 } else {
   const message = ".env variable `REACT_APP_API_GA` is empty, GA will not be initialized."
-  alert(message)
   console.warn(message)
 }
 
