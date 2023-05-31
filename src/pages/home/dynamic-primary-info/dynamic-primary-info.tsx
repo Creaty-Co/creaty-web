cc71054d9500e7133b6a621f9e4455d21df1d553
Dmitrii Doroshenko <DmitriyD@radware.com>
2023-05-31 20:05:37 +0300

    fixes

--- src/pages/home/dynamic-primary-info/dynamic-primary-info.tsx
+++ src/pages/home/dynamic-primary-info/dynamic-primary-info.tsx
@@ -62,15 +62,11 @@ export function DynamicPrimaryInfo(props: IDynamicPrimaryInfo) {
   }
 
   useEffect(() => {
-    ;(async () => {
+    (async () => {
       if (topics.list.length === 0) return
       if (props.firstHeadingShortcut) {
-        const topic = topics.list.find(
-          (topic: { shortcut: string | undefined }) => topic.shortcut === props.firstHeadingShortcut
-        )
-        const tag = topics.tags.find(
-          (tag: { shortcut: string | undefined }) => tag.shortcut === props.firstHeadingShortcut
-        )
+        const topic = topics.list.find((topic: { shortcut: string | undefined }) => topic.shortcut === props.firstHeadingShortcut)
+        const tag = topics.tags.find((tag: { shortcut: string | undefined }) => tag.shortcut === props.firstHeadingShortcut)
         const heading = topic?.title || tag?.title
 
         if (heading) {
@@ -95,7 +91,9 @@ export function DynamicPrimaryInfo(props: IDynamicPrimaryInfo) {
         <span>{t("title")}</span>
       </div>
 
-      <div className={getElement("desc")}>{t("desc")}</div>
+      <div className={getElement("desc")}>
+        {t("desc")}
+      </div>
     </div>
   )
 }
