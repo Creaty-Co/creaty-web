801e698e3266d2baf8a91cbca55101082b96a030
Dmitrii Doroshenko <DmitriyD@radware.com>
2023-06-09 17:48:13 +0300

    fixes

--- src/features/mentor-search/mentor-search-list.tsx
+++ src/features/mentor-search/mentor-search-list.tsx
@@ -5,12 +5,11 @@ import { CategoryType, Tag, TagType } from "@entities"
 import { selectIsMobile } from "@entities/device"
 import { Icon, LoaderCover } from "@shared/ui"
 import { bem, toDataAttrs } from "@shared/utils"
-import { Key } from "react"
 import { useTranslation } from "react-i18next"
 
 import { MentorSearchListItem } from "./mentor-search-list-item"
 
-/* BEM */ 
+/* BEM */
 const CN = "mentor-search-list"
 const { getElement, getModifier } = bem(CN)
 
@@ -23,164 +22,148 @@ interface MentorSearchListProps {
   pureSearch: boolean
 }
 
-export function MentorSearchList({
-  searchState,
-  pureSearch,
-  topics,
-  value
-}: MentorSearchListProps) {
+export function MentorSearchList({ searchState, pureSearch, topics, value }: MentorSearchListProps) {
   const isMobile = useAppSelector(selectIsMobile)
   const { t } = useTranslation("translation", { keyPrefix: "views.home.mentorSearch" })
 
-  const categories = value === null || value.length === 0
-    ? topics.list
-    : topics.list.filter((t: { title: string }) => t.title.toLocaleLowerCase().search(value.toLocaleLowerCase()) !== -1)
+  const categories =
+    value === null || value.length === 0
+      ? topics.list
+      : topics.list.filter(
+          (t: { title: string }) => t.title.toLocaleLowerCase().search(value.toLocaleLowerCase()) !== -1
+        )
   /* console.log("categories", categories) */
 
-  const tags = value === null || value.length === 0
-    ? searchState.topic?.tags || topics.tags
-    : topics.tags.filter((t: { title: string }) => t.title.toLocaleLowerCase().search(value.toLocaleLowerCase()) !== -1)
+  const tags =
+    value === null || value.length === 0
+      ? searchState.topic?.tags || topics.tags
+      : topics.tags.filter(
+          (t: { title: string }) => t.title.toLocaleLowerCase().search(value.toLocaleLowerCase()) !== -1
+        )
   /* console.log("tags", tags) */
-
 
   // console.log("NOUNT")
   return (
     <div
-      className={getModifier(CN,
+      className={getModifier(
+        CN,
         searchState.focused && "visible",
         (searchState.topic || searchState.tag) && "selected"
       )}
     >
       {/* Selected only in Mobil*/}
-      {isMobile && (searchState.topic || searchState.tag) && 
-          <div className={getElement("selected")}>
-            {/* if selected a topic */}
-            {searchState.topic &&
-              <MentorSearchListItem 
-                topic={searchState.topic}
-                state="selected"
-                
-                className={getElement("item")} 
+      {isMobile && (searchState.topic || searchState.tag) && (
+        <div className={getElement("selected")}>
+          {/* if selected a topic */}
+          {searchState.topic && (
+            <MentorSearchListItem
+              topic={searchState.topic}
+              state="selected"
+              className={getElement("item")}
+              dataAttrsIcon={toDataAttrs({
+                selector: "topic",
+                action: "remove",
+                id: searchState.topic.id.toString(),
+              })}
+            />
+          )}
 
-                dataAttrsIcon={toDataAttrs({
-                  "selector": "topic",
-                  "action": "remove",
-                  "id": searchState.topic.id.toString()
-                })}
-              />
-            }
+          {/* if selected a tag */}
+          {searchState.tag && (
+            <Tag
+              key={searchState.tag.id}
+              dataAttrs={toDataAttrs({
+                selector: "tag",
+                action: "remove",
+                id: searchState.tag.id.toString(),
+              })}
+            >
+              {searchState.tag}
+            </Tag>
+          )}
 
-            {/* if selected a tag */}
-            {searchState.tag &&
-              <Tag key={searchState.tag.id}
-                dataAttrs={toDataAttrs({
-                  "selector": "tag",
-                  "action": "remove",
-                  "id": searchState.tag.id.toString()
-                })}
-              >
-                {searchState.tag}
-              </Tag>
-            }
-
-            {/* if selected a topic but not a tag */}
-            {searchState.topic && !searchState.tag &&
-              <MentorSearchListItem
-                topic={searchState.topic}
-                type="view-all"
-
-                className={getElement("item")}
-              />
-            }
-          </div>
-      }
+          {/* if selected a topic but not a tag */}
+          {searchState.topic && !searchState.tag && (
+            <MentorSearchListItem topic={searchState.topic} type="view-all" className={getElement("item")} />
+          )}
+        </div>
+      )}
 
       {/* Separator */}
       <div className={getModifier(getElement("separator"), "only-for-mobile")}></div>
 
       {/* Empty search results for all. Only mobile */}
-      {isMobile && value !== null && value.length > 0 && 
-          categories.length === 0 && tags.length === 0 &&          
-            <div className={getModifier(getElement("search-result"), "all")}>NO ONE SEARCH RESULTS</div>
-      }
+      {isMobile && value !== null && value.length > 0 && categories.length === 0 && tags.length === 0 && (
+        <div className={getModifier(getElement("search-result"), "all")}>NO ONE SEARCH RESULTS</div>
+      )}
 
       {/* Categories */}
-      {(!isMobile || 
-        !searchState.topic || 
-        (searchState.topic && pureSearch)
-      ) && <div className={getElement("categories")}>
-        {/* Selected categories only on desktop */}
-        {!isMobile && searchState.topic && 
-          <>
-            <MentorSearchListItem 
-              topic={searchState.topic}
-              state="selected"
-            
-              className={getElement("item")} 
+      {(!isMobile || !searchState.topic || (searchState.topic && pureSearch)) && (
+        <div className={getElement("categories")}>
+          {/* Selected categories only on desktop */}
+          {!isMobile && searchState.topic && (
+            <>
+              <MentorSearchListItem
+                topic={searchState.topic}
+                state="selected"
+                className={getElement("item")}
+                dataAttrsIcon={toDataAttrs({
+                  selector: "topic",
+                  action: "remove",
+                  id: searchState.topic.id.toString(),
+                })}
+              />
 
-              dataAttrsIcon={toDataAttrs({
-                "selector": "topic",
-                "action": "remove",
-                "id": searchState.topic.id.toString()
-              })}
-            />
+              <MentorSearchListItem topic={searchState.topic} type="view-all" className={getElement("item")} />
 
-            <MentorSearchListItem
-              topic={searchState.topic}
-              type="view-all"
+              <div className={getElement("separator")}></div>
+            </>
+          )}
 
-              className={getElement("item")}
-            />
-
-            <div className={getElement("separator")}></div>
-          </>
-        }
-      
-        {/* Empty search results for categories. Only desktop */}
-        {!isMobile && value !== null && value.length > 0 && 
-          categories.length === 0 &&          
+          {/* Empty search results for categories. Only desktop */}
+          {!isMobile && value !== null && value.length > 0 && categories.length === 0 && (
             <div className={getModifier(getElement("search-result"), "categories")}>No avaliable categories</div>
-        }
+          )}
 
-        {/* All topics */}
-        {
-          categories.filter((t: { id: any }) => t.id !== searchState.topic?.id).map((topic: CategoryType) => 
-            <MentorSearchListItem key={topic.id} 
-              {...{topic}}
+          {/* All topics */}
+          {categories
+            .filter((t: { id: any }) => t.id !== searchState.topic?.id)
+            .map((topic: CategoryType) => (
+              <MentorSearchListItem
+                key={topic.id}
+                {...{ topic }}
+                dataAttrs={toDataAttrs({
+                  selector: "topic",
+                  action: "add",
+                  id: topic?.id?.toString() || "",
+                })}
+              />
+            ))}
 
-              dataAttrs={toDataAttrs({
-                "selector": "topic",
-                "action": "add",
-                "id": topic?.id?.toString() || ""
-              })}
-            />
-          )
-        }
-
-        {/* Loader */}
-        {topics.list.length === 0 && 
-          <LoaderCover />
-        }
-      </div> }
+          {/* Loader */}
+          {topics.list.length === 0 && <LoaderCover />}
+        </div>
+      )}
 
       {/* Tags */}
       <div
-        className={getModifier(getElement("tags"),
-          isMobile && 
-          (value === null || value.length === 0) && (
-            (searchState.topic && pureSearch) || 
-            !searchState.topic
-          ) && "hidden"
+        className={getModifier(
+          getElement("tags"),
+          isMobile &&
+            (value === null || value.length === 0) &&
+            ((searchState.topic && pureSearch) || !searchState.topic) &&
+            "hidden"
         )}
       >
         {/* Selected only on desktop */}
-        {!isMobile && searchState.tag && 
+        {!isMobile && searchState.tag && (
           <>
-            <Tag key={searchState.tag.id}
+            <Tag
+              key={searchState.tag.id}
               dataAttrs={toDataAttrs({
-                "selector": "tag",
-                "action": "remove",
-                "id": searchState.tag.id.toString()
+                selector: "tag",
+                action: "remove",
+                id: searchState.tag.id.toString(),
               })}
             >
               {searchState.tag}
@@ -188,35 +171,35 @@ export function MentorSearchList({
 
             <div className={getElement("separator")}></div>
           </>
-        }
+        )}
 
         {/* Empty search results for tags. Only desktop */}
-        {!isMobile && value !== null && value.length > 0 && 
-        tags.length === 0 &&          
+        {!isMobile && value !== null && value.length > 0 && tags.length === 0 && (
           <div className={getModifier(getElement("search-result"), "tags")}>No avaliable categories</div>
-        }
+        )}
 
         {/* Tags of topic */}
-        {tags.filter((tag: TagType) => tag.id !== searchState.tag?.id)
+        {tags
+          .filter((tag: TagType) => tag.id !== searchState.tag?.id)
           .filter((tag: TagType) => tag.title)
-          .map((tag: TagType) => 
-            <Tag key={tag.id}
+          .map((tag: TagType) => (
+            <Tag
+              key={tag.id}
               dataAttrs={toDataAttrs({
-                "selector": "tag",
-                "action": "add",
-                "id": tag.id.toString()
+                selector: "tag",
+                action: "add",
+                id: tag.id.toString(),
               })}
             >
               {tag}
             </Tag>
-          )}
+          ))}
 
         {/* Empty tags */}
-        {!searchState.topic && false &&
+        {!searchState.topic && false && (
           <MentorSearchListIndicatorEmpty>{t("chooseTopic")}</MentorSearchListIndicatorEmpty>
-        }
+        )}
       </div>
-
     </div>
   )
 }
