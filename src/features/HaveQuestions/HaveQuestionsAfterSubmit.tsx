42eab37038f6c64231405ff5b3322df1d09cc31e
Dmitrii <lendmid@bk.ru>
2023-07-31 13:51:26 +0300

    migrated PopupFormThanks to i18n (#171)

--- src/features/HaveQuestions/HaveQuestionsAfterSubmit.tsx
+++ src/features/HaveQuestions/HaveQuestionsAfterSubmit.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@shared/ui"
 import { bem } from "@shared/utils"
 import cn from "classnames"
+import { useTranslation } from "react-i18next"
 
 const CN = "have-questions"
 const { getElement } = bem(CN)
@@ -12,13 +13,13 @@ interface IProps {
 }
 
 export const HaveQuestionsAfterSubmit = ({ reset }: IProps) => {
+  const { t } = useTranslation("translation", { keyPrefix: "other.forms.popupFormThanks" })
+
   return (
     <div className={cn(CNElement, "grid grid-cols-1 grid-rows-[auto_1fr] gap-3")}>
-      <div className={cn(getElement("title"), "font--h3-bold text-black-900")}>Thanks for request!</div>
+      <div className={cn(getElement("title"), "font--h3-bold text-black-900")}>{t("title")}</div>
 
-      <div className={cn(getElement("desc"), "font--text-regular text-black-900")}>
-        We will contact you within 24 hours to help you find a perfect mentor :)
-      </div>
+      <div className={cn(getElement("desc"), "font--text-regular text-black-900")}>{t("body")}</div>
 
       <Button size="biggest" color="dark" type="submit" onClick={reset}>
         Reset
