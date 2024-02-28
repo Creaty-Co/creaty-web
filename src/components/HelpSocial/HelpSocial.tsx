354d333636d3211119e790fa5832fcaafedd69fa
Dmitrii Doroshenko <lendmid@gmail.com>
2024-02-28 21:48:02 +0200

    updated icons

--- src/components/HelpSocial/HelpSocial.tsx
+++ src/components/HelpSocial/HelpSocial.tsx
@@ -17,17 +17,17 @@ export function HelpSocial() {
 
       <div>
         <OuterLink to="http://m.me/103451835157882/">
-          <img className={getElement("icon")} alt="social network" src="/static/icons/social-facebook-messenger.svg" />
+          <img className={getElement("icon")} alt="social network" src="/static/icons/messenger.svg" />
         </OuterLink>
       </div>
       <div>
         <OuterLink to="https://wa.me/972533231644">
-          <img className={getElement("icon")} alt="social network" src="/static/icons/social-whatsapp-messenger.svg" />
+          <img className={getElement("icon")} alt="social network" src="/static/icons/whatsapp.svg" />
         </OuterLink>
       </div>
       <div>
         <OuterLink to="https://t.me/CreatyClub">
-          <img className={getElement("icon")} alt="social network" src="/static/icons/social-telegram-messenger.svg" />
+          <img className={getElement("icon")} alt="social network" src="/static/icons/telegram.svg" />
         </OuterLink>
       </div>
     </div>
