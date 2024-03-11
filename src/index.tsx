e2b8d8ab446f6b977a4934423ff6dbf874aef635
Dmitrii Doroshenko <DmitriyD@radware.com>
2024-03-11 12:46:22 +0200

    improve virtualKeyboard behaviour

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import ReactDOM from "react-dom/client"
 import { App } from "./app/App"
 
 initI18()
+if ("virtualKeyboard" in navigator) {
+  (window.navigator as any).virtualKeyboard.overlaysContent = true
+}
 const rootElement = document.getElementById("root")
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement)
